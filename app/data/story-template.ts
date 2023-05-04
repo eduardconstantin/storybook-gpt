37b166f02e43301f35e5361fcce3a0a0dea18648
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2023-05-04 17:04:21 +0000

    Fix template

--- app/data/story-template.ts
+++ app/data/story-template.ts
@@ -1,20 +1,4 @@
-export const template = `import type { Meta, StoryObj } from '@storybook/react';
-//import component
-const meta: Meta<//type of component> = {  
-  title: //title of component,  
-  component: //component ,
-};
-export default meta;
-type Story = StoryObj<//type of component>;
-const StoryTemplate: Story = {  
-  render: (args) => //render component,
-};
-export Primary = {  
-  ...StoryTemplate,  
-  args: {      
-    //component's props  
-  }
-}`;
+export const template = `import type { Meta, StoryObj } from '@storybook/react'; //import component const meta: Meta<//type of component> = {  title: //title of component,  component: //component ,};export default meta;type Story = StoryObj<//type of component>;const StoryTemplate: Story = {  render: (args) => //render component,};export Primary = {  ...StoryTemplate,  args: {      //component's props  }}`;
 
 export const component =
   "import React, { useState } from 'react';const CounterButton = ({handleClick}) => {  const [count, setCount] = useState(0);  return (    <button onClick={handleClick}>      {`Clicked ${count} time${count === 1 ? '' : 's'}`}    </button>  );};export default CounterButton;";
