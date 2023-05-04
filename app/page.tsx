df15073f9e98a02ee6fc09814a25509b0984f2a1
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2023-05-04 18:53:45 +0000

    Add form component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import React from "react";
 import { Configuration, OpenAIApi } from "openai";
+import { Form } from "./form";
 import { template } from "./data/story-template";
 
 export async function ComponentConverter({ component, apiKey }: { component: string; apiKey: string }) {
+  "use server";
   const prompt = `Write a Storybook component for a React component, without any comments added. Here's the code for the input component:\n ${component}\n This is the template you should use for the storybook story, keep the provided format, add component variants if possible:\n ${template}`;
 
   const configuration = new Configuration({
@@ -25,7 +28,9 @@ export async function ComponentConverter({ component, apiKey }: { component: str
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <Form convertComponent={ComponentConverter} />
+      </div>
     </main>
   );
 }
