d34106f9ac309897d24367c70713faa4f1e56f8b
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2023-05-04 16:11:33 +0000

    Fetch data from api

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ['latin'] })
+async function getData() {
+  const res = await fetch("/api/storybook-gpt");
 
-export default function Home() {
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-
-
-      </div>
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
     </main>
-  )
+  );
 }
