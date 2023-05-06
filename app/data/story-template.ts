36091789c1c6d6f4d9a7efc2bf4ae24164ed435d
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2023-05-06 13:23:39 +0000

    Improve template

--- app/data/story-template.ts
+++ app/data/story-template.ts
@@ -1,4 +1,1 @@
-export const template = `import type { Meta, StoryObj } from '@storybook/react'; //import component const meta: Meta<//type of component> = {  title: //title of component,  component: //component ,};export default meta;type Story = StoryObj<//type of component>;const StoryTemplate: Story = {  render: (args) => //render component,};export Primary = {  ...StoryTemplate,  args: {      //component's props  }}`;
-
-export const component =
-  "import React, { useState } from 'react';const CounterButton = ({handleClick}) => {  const [count, setCount] = useState(0);  return (    <button onClick={handleClick}>      {`Clicked ${count} time${count === 1 ? '' : 's'}`}    </button>  );};export default CounterButton;";
+export const template = `import type { Meta, StoryObj } from '@storybook/react';\n//import component\nconst meta: Meta<//type of component> = {\n title: //title of component,\n component: //component\n};\nexport default meta;\ntype Story = StoryObj<//type of component>;\nconst StoryTemplate: Story = {\n render: (args) => //render component\n};\nexport Primary = {\n ...StoryTemplate,\n args: {\n //component's props\n}\n}`;
