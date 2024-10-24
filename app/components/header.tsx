27d1bc9ed9b63295141aaa45f46401ddc3a342b0
its-Pratik-15 <panpratik15@gmail.com>
2024-10-24 23:50:44 +0530

    the contact details is missing in the website #40
    Added owners contact details  .

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -14,7 +14,24 @@ const Header = () => {
           Generate Storybook stories from React components
         </p>
       </div>
-      <div className="flex items-center gap-5 ">
+      <div className="flex items-center gap-5 "><div class="social-links">
+    <!-- Email -->
+    <a href="ibinceanu.eduard@yahoo.com" target="_blank">
+        <img src="https://img.icons8.com/fluent/48/000000/email-open.png" alt="Email">
+    </a>
+    
+    <!-- GitHub -->
+    <a href="eduardconstantin.github.io" target="_blank">
+        <img src="https://img.icons8.com/fluent/48/000000/github.png" alt="GitHub">
+    </a>
+    
+    <!-- Twitter -->
+    <a href="https://x.com/_Eduard26" target="_blank">
+        <img src="https://img.icons8.com/fluent/48/000000/twitter.png" alt="Twitter">
+    </a>
+    
+ 
+</div>
         <DarkModeSwitch />
         <GitHubButton
           href="https://github.com/eduardconstantin/storybook-gpt"
