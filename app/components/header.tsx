1fcc33238f0fc516f005a077f67401f7cebe97ac
Pratik Kumar Pan <panpratik15@gmail.com>
2024-10-27 13:47:33 +0530

    the contact details is missing in the website #40 #41
    done changes

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -15,22 +15,6 @@ const Header = () => {
         </p>
       </div>
       <div className="flex items-center gap-5 ">
-      <div className="social-links">
-          {/* Email */}
-          <a href="mailto:ibinceanu.eduard@yahoo.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/48/000000/email-open.png" alt="Email" />
-          </a>
-
-          {/* GitHub */}
-          <a href="https://eduardconstantin.github.io" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/48/000000/github.png" alt="GitHub" />
-          </a>
-
-          {/* Twitter */}
-          <a href="https://x.com/_Eduard26" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/48/000000/twitter.png" alt="Twitter" />
-          </a>
-      </div>
         <DarkModeSwitch />
         <GitHubButton
           href="https://github.com/eduardconstantin/storybook-gpt"
@@ -42,6 +26,29 @@ const Header = () => {
         >
           Star
         </GitHubButton>
+      </div>
+      <div className="flex items-center gap-4 mt-4">
+        <a href="mailto:ibinceanu.eduard@yahoo.com" target="_blank">
+          <img
+            src="https://img.icons8.com/fluent/32/000000/email-open.png"
+            alt="Email"
+            className="h-6 w-6"
+          />
+        </a>
+        <a href="https://eduardconstantin.github.io" target="_blank">
+          <img
+            src="https://img.icons8.com/fluent/32/000000/github.png"
+            alt="GitHub"
+            className="h-6 w-6"
+          />
+        </a>
+        <a href="https://x.com/_Eduard26" target="_blank">
+          <img
+            src="https://img.icons8.com/fluent/32/000000/twitter.png"
+            alt="Twitter"
+            className="h-6 w-6"
+          />
+        </a>
       </div>
     </div>
   )
